d20fb23b52afa9ad2a5d1ceb2a6e16cfb551cca5
Swetha Murugesan <swethamk45@gmail.com>
2025-06-24 10:43:59 +0530

    updated

---
+++ calendar/src/App.jsx
@@ -0,0 +1,7 @@
+// src/App.jsx
+import React from "react";
+import Calendar from "./components/Calendar";
+
+export default function App() {
+  return <Calendar />;
+}
---
+++ calendar/src/components/CalendarUtils.jsx
@@ -0,0 +1,30 @@
+import dayjs from "dayjs";
+
+export const generateDate = (month = dayjs().month(), year = dayjs().year()) => {
+  const firstDateOfMonth = dayjs().year(year).month(month).startOf("month");
+  const lastDateOfMonth = dayjs().year(year).month(month).endOf("month");
+  const arrayOfDate = [];
+
+  for (let i= 0;i<(firstDateOfMonth.day()+6) %7;i++) {
+    arrayOfDate.push({ currentMonth: false, date: firstDateOfMonth.subtract((firstDateOfMonth.day() + 6) % 7 - i, "day") });
+  }
+  for (let i=1;i<=lastDateOfMonth.date();i++) {
+    const date = dayjs().year(year).month(month).date(i);
+    arrayOfDate.push({
+      currentMonth: true,
+      date,
+      today: date.isSame(dayjs(), "day"),
+    });
+  }
+  const remaining = 42 - arrayOfDate.length;
+  for (let i = 1; i <= remaining; i++) {
+    arrayOfDate.push({ currentMonth: false, date: lastDateOfMonth.add(i, "day") });
+  }
+
+  return arrayOfDate;
+};
+
+export const months = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
